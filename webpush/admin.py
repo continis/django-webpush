935315284b7ac8819f4846ccf6bd0f00b539b2ec
Julian Wachholz <jw@continis.ch>
2026-04-22 13:56:44 +0200

    fix test message payload

--- webpush/admin.py
+++ webpush/admin.py
@@ -1,6 +1,8 @@
 import json
 
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
 
 from .models import PushInformation
@@ -12,14 +14,31 @@ class PushInfoAdmin(admin.ModelAdmin):
 
     actions = ("send_test_message",)
 
+    readonly_fields = ("subscription_details",)
+
+    def subscription_details(self, obj):
+        data = json.dumps(
+            {
+                "user_agent": obj.subscription.user_agent,
+                "endpoint": obj.subscription.endpoint,
+                "keys": {
+                    "auth": obj.subscription.auth,
+                    "p256dh": obj.subscription.p256dh,
+                },
+            },
+            indent=4,
+        )
+        return format_html("<code><pre>{}</pre></code>", data)
+
     def send_test_message(self, request, queryset):
-        payload = {"head": _("Hey"), "body": _("Hello World")}
+        payload = {"head": gettext("Hey"), "body": gettext("Hello World")}
         for device in queryset:
             notification = _send_notification(device.subscription, json.dumps(payload), 0)
             if notification:
                 self.message_user(request, _("Test sent successfully"))
             else:
                 self.message_user(request, _("Deprecated subscription deleted"))
+
     send_test_message.short_description = _("Send test message")
 
 
